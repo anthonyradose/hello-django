8905e538a1f53ef5de68b6141120f063d5acd9e4
Anthony Radose <asradose@gmail.com>
2024-04-12 13:38:22 +0000

    Fixed Allowed_hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$*=m3srcx0$l5oa=!h6s^j+l%hix*yfmaut+l!^jlzy&_sg_11
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-anthonyrado-hellodjango-df3i3oni5s7.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['asradose-django-todo-app.herokuapp.com']
 
 
 # Application definition
